e89d7e885d831520cdaa20bdbc4ca517fc4ff5fc
Alan Tsai <alan@alantsai.net>
2018-09-28 18:30:09 +0800

    加入常用參數

--- src/classification/MHAT.MLNET.Classifier.IssueLabeler.Trainer/Program.cs
+++ src/classification/MHAT.MLNET.Classifier.IssueLabeler.Trainer/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.IO;
 
 namespace MHAT.MLNET.Classifier.IssueLabeler.Trainer
 {
     class Program
     {
+        private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+
+        private static string DataPath => Path.Combine(AppPath, "Data", "corefx-issues-train.tsv");
+
+        private static string TestPath => Path.Combine(AppPath, "Data", "corefx-issues-test.tsv");
+
+        private static string ModelPath => Path.Combine(AppPath, "IssueLabelerModel.zip");
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
