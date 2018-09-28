0d0d2f4b80e7b2a272d995acc28cf0abbcb47b7e
Alan Tsai <alan@alantsai.net>
2018-09-28 18:27:52 +0800

    建立出代表資料以及prediction的model

---
+++ src/classification/MHAT.MLNET.Classifier.IssueLabeler.Core/Model/GitHubIssue.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MHAT.MLNET.Classifier.IssueLabeler.Core.Model
+{
+    public class GitHubIssue
+    {
+        [Column(ordinal: "0")]
+        public string ID;
+
+        [Column(ordinal: "1")]
+        public string Area;
+
+        [Column(ordinal: "2")]
+        public string Title;
+
+        [Column(ordinal: "3")]
+        public string Description;
+    }
+}
---
+++ src/classification/MHAT.MLNET.Classifier.IssueLabeler.Core/Model/GithubIssueLabelPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MHAT.MLNET.Classifier.IssueLabeler.Core.Model
+{
+    public class GithubIssueLabelPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string Area;
+    }
+}
