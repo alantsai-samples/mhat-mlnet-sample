7073a0ac83e0a27f8629a536efac45f797852301
Alan Tsai <alan@alantsai.net>
2018-09-28 18:48:58 +0800

    定義出使用模型的畫面

--- src/classification/MHAT.MLNET.Classifier.IssueLabeler.WinForm/Form1.Designer.cs
+++ src/classification/MHAT.MLNET.Classifier.IssueLabeler.WinForm/Form1.Designer.cs
@@ -28,13 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblModelPath = new System.Windows.Forms.Label();
+            this.txtModelPath = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.rtxtboxDescription = new System.Windows.Forms.RichTextBox();
+            this.btnPredic = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblModelPath
+            // 
+            this.lblModelPath.AutoSize = true;
+            this.lblModelPath.Location = new System.Drawing.Point(36, 24);
+            this.lblModelPath.Name = "lblModelPath";
+            this.lblModelPath.Size = new System.Drawing.Size(89, 18);
+            this.lblModelPath.TabIndex = 0;
+            this.lblModelPath.Text = "Model路徑";
+            // 
+            // txtModelPath
+            // 
+            this.txtModelPath.Location = new System.Drawing.Point(141, 21);
+            this.txtModelPath.Name = "txtModelPath";
+            this.txtModelPath.Size = new System.Drawing.Size(501, 28);
+            this.txtModelPath.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(36, 99);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(44, 18);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "標題";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(141, 89);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(501, 28);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(36, 162);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(44, 18);
+            this.lblDescription.TabIndex = 0;
+            this.lblDescription.Text = "描述";
+            // 
+            // rtxtboxDescription
+            // 
+            this.rtxtboxDescription.Location = new System.Drawing.Point(141, 159);
+            this.rtxtboxDescription.Name = "rtxtboxDescription";
+            this.rtxtboxDescription.Size = new System.Drawing.Size(501, 213);
+            this.rtxtboxDescription.TabIndex = 2;
+            this.rtxtboxDescription.Text = "";
+            // 
+            // btnPredic
+            // 
+            this.btnPredic.Location = new System.Drawing.Point(529, 389);
+            this.btnPredic.Name = "btnPredic";
+            this.btnPredic.Size = new System.Drawing.Size(113, 49);
+            this.btnPredic.TabIndex = 3;
+            this.btnPredic.Text = "預測";
+            this.btnPredic.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPredic);
+            this.Controls.Add(this.rtxtboxDescription);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtModelPath);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblModelPath);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblModelPath;
+        private System.Windows.Forms.TextBox txtModelPath;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.RichTextBox rtxtboxDescription;
+        private System.Windows.Forms.Button btnPredic;
     }
 }
 
