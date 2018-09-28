16dc89b4baeb8a52bf34753c4a38257d91cb6c16
Alan Tsai <alan@alantsai.net>
2018-09-28 18:36:39 +0800

    準備訓練步奏的執行内容

--- src/classification/MHAT.MLNET.Classifier.IssueLabeler.Trainer/Program.cs
+++ src/classification/MHAT.MLNET.Classifier.IssueLabeler.Trainer/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using MHAT.MLNET.Classifier.IssueLabeler.Core.Model;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -23,7 +28,30 @@ namespace MHAT.MLNET.Classifier.IssueLabeler.Trainer
 
         private static async Task TrainAsync()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("========準備訓練資料=============");
+
+            var pipeline = new LearningPipeline();
+
+            pipeline.Add(new TextLoader(DataPath).CreateFrom<GitHubIssue>(useHeader: true));
+
+            // 把Area轉換成為Dictionary數字
+            pipeline.Add(new Dictionarizer(("Area", "Label")));
+
+            // 把兩個用來訓練的欄位變成數字的vector
+            pipeline.Add(new TextFeaturizer("Title", "Title"));
+            pipeline.Add(new TextFeaturizer("Description", "Description"));
+
+            // Title 和 Description合并變成訓練的闌尾
+            pipeline.Add(new ColumnConcatenator("Features", "Title", "Description"));
+
+            // 使用StochasticDualCoordinateAscent演算法
+            pipeline.Add(new StochasticDualCoordinateAscentClassifier());
+
+            // 把判斷出來的數字轉回文字版本
+            pipeline.Add(new PredictedLabelColumnOriginalValueConverter()
+                { PredictedLabelColumn = "PredictedLabel" });
+
+
         }
     }
 }
