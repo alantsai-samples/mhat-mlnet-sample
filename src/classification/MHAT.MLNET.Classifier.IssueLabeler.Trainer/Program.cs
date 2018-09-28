3162d070b36b28cd2159a6a7a0863f1865389e9c
Alan Tsai <alan@alantsai.net>
2018-09-28 18:21:55 +0800

    init trainer console

---
+++ src/classification/MHAT.MLNET.Classifier.IssueLabeler.Trainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MHAT.MLNET.Classifier.IssueLabeler.Trainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
